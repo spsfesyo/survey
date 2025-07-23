ae78de23dfa9258f3d0d0ab49c7d8ec8598af8b8
yogaadityaw <yogaaawi12345@gmail.com>
2025-07-23 10:17:53 +0700

    ubah logo di auth header

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,5 +1,5 @@
  <div class="login-brand">
-     <img src="{{ asset('img/logo_blesscon.svg') }}"
+     <img src="{{ asset('img/logo-superior-prima-sukses-no-bg.png') }}"
          alt="logo"
          width="300">
  </div>
